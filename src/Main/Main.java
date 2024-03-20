f3529ae9733fdaef16db7d0df318a966249001d6
Lolel_ <lelemodr@gmail.com>
2024-03-20 11:41:59 +0100

    test lab commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Questo è il lab!");
+        System.out.println("Lab!");
 
         Map m = new Map();
 
